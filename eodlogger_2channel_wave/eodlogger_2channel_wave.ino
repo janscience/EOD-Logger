182406315221ff90e82ee998037494ef151ef059
Jan Benda <jan.benda@uni-tuebingen.de>
2022-04-10 23:07:41 +0200

    updated needToWrite() to pending()

--- eodlogger_2channel_wave/eodlogger_2channel_wave.ino
+++ eodlogger_2channel_wave/eodlogger_2channel_wave.ino
@@ -87,7 +87,7 @@ void setupStorage() {
 
 
 void storeData() {
-  if (file.needToWrite()) {
+  if (file.pending()) {
     size_t samples = file.write();
     if (samples == 0) {
       blink.clear();
