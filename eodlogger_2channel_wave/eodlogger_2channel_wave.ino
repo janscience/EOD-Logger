bd7cb105badef45f0e3ba2b058f66a3e4ff2b975
Jan Benda <jan.benda@uni-tuebingen.de>
2022-04-20 10:41:35 +0200

    improved error handling of file issues

--- eodlogger_2channel_wave/eodlogger_2channel_wave.ino
+++ eodlogger_2channel_wave/eodlogger_2channel_wave.ino
@@ -33,8 +33,11 @@ ContinuousADC aidata;
 SDCard sdcard;
 SDWriter file(sdcard, aidata);
 Settings settings("recordings", fileName, fileSaveTime, pulseFrequency);
+String prevname; // previous file name
 RTClock rtclock;
 Blink blink(LED_BUILTIN);
+
+int restarts = 0;
 
 
 void setupADC() {
@@ -49,36 +52,48 @@ void setupADC() {
 }
 
 
-void openNextFile() {
+bool openNextFile() {
+  blink.clear();
   time_t t = now();
   String name = rtclock.makeStr(settings.FileName, t, true);
+  if (name != prevname) {
+    file.resetFileCounter();
+    prevname = name;
+  }
   name = file.incrementFileName(name);
   if (name.length() == 0) {
-    Serial.println("WARNING: failed to open file on SD card.");
-    Serial.println("SD card probably not inserted.");
+    Serial.println("WARNING: failed to increment file name.");
+    Serial.println("SD card probably not inserted -> halt");
     Serial.println();
-    return;
+    aidata.stop();
+    while (1) {};
+    return false;
   }
   name += ".wav";
   char dts[20];
   rtclock.dateTime(dts, t);
-  file.openWave(name.c_str(), -1, dts);
-  file.write();
-  Serial.println(name);
-  if (file.isOpen()) {
-    blink.set(5000, 200);
-    blink.blink(2000, 1000);
-  }
-  else {
+  if (! file.openWave(name.c_str(), -1, dts)) {
     Serial.println();
     Serial.println("WARNING: failed to open file on SD card.");
-    Serial.println("SD card probably not inserted.");
+    Serial.println("SD card probably not inserted or full -> halt");
+    aidata.stop();
+    while (1) {};
+    return false;
   }
+  file.write();
+  Serial.println(name);
+  blink.setSingle();
+  blink.blinkSingle(0, 1000);
+  return true;
 }
 
 
 void setupStorage() {
-  file.dataDir(settings.Path);
+  prevname = "";
+  if (settings.FileTime > 30)
+    blink.setTiming(5000);
+  if (file.dataDir(settings.Path))
+    Serial.printf("Save recorded data in folder \"%s\".\n\n", settings.Path);
   file.setWriteInterval();
   file.setMaxFileTime(settings.FileTime);
   file.start();
@@ -94,19 +109,47 @@ void storeData() {
       Serial.println();
       Serial.println("ERROR in writing data to file:");
       switch (samples) {
-        case -1: Serial.println("  File not open."); break;
-        case -2: Serial.println("  No data available, data acquisition probably not running."); break;
-        case -3: Serial.println("  File already full."); break;
-        case 0: Serial.println("  Nothing written into the file."); break;
-        default: Serial.printf("  Unknown error %d.\n", samples);
+        case 0:
+          Serial.println("  Nothing written into the file.");
+          Serial.println("  SD card probably full -> halt");
+          aidata.stop();
+          while (1) {};
+          break;
+        case -1:
+          Serial.println("  File not open.");
+          break;
+        case -2:
+          Serial.println("  File already full.");
+          break;
+        case -3:
+          Serial.println("  No data available, data acquisition probably not running.");
+          Serial.println("  sampling rate probably too high,");
+          Serial.println("  given the number of channels, averaging, sampling and conversion speed.");
+          break;
       }
-      Serial.println("sampling rate probably too high,");
-      Serial.println("given the number of channels, averaging, sampling and conversion speed.");
-      while(1) {};
+      if (samples == -3) {
+        aidata.stop();
+        file.closeWave();
+        char mfs[20];
+        sprintf(mfs, "error%d-%d.msg", restarts+1, -samples);
+        FsFile mf = sdcard.openWrite(mfs);
+        mf.close();
+      }
     }
-    if (file.endWrite()) {
-      file.close();
-      blink.clear();
+    if (file.endWrite() || samples < 0) {
+      file.close();  // file size was set by openWave()
+      if (samples < 0) {
+        restarts++;
+        if (restarts >= 5) {
+          Serial.println("ERROR: Too many file errors -> halt.");
+          aidata.stop();
+          while (1) {};
+        }
+      }
+      if (samples == -3) {
+        aidata.start();
+        file.start();
+      }
       openNextFile();
     }
   }
