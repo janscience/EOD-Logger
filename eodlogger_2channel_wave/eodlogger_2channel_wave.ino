9edcf8b9ec34825072074cc2418cceba3e620bfc
Jan Benda <jan.benda@uni-tuebingen.de>
2022-12-27 17:15:33 +0100

    renamed ContinuousADC to TeensyADC

--- eodlogger_2channel_wave/eodlogger_2channel_wave.ino
+++ eodlogger_2channel_wave/eodlogger_2channel_wave.ino
@@ -1,5 +1,5 @@
 #include <Configurator.h>
-#include <ContinuousADC.h>
+#include <TeensyADC.h>
 #include <SDWriter.h>
 #include <RTClock.h>
 #include <Settings.h>
@@ -36,7 +36,7 @@ RTClock rtclock;
 Configurator config;
 
 DATA_BUFFER(AIBuffer, NAIBuffer, 256*256)
-ContinuousADC aidata(AIBuffer, NAIBuffer);
+TeensyADC aidata(AIBuffer, NAIBuffer);
 
 SDCard sdcard;
 SDWriter file(sdcard, aidata);
